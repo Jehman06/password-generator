5b276511568413dc29dd1e4618ff5da2f8e25b6c
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-09-01 22:23:25 -0700

    Added Copy and Re-generate feature

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import secrets
 import string
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -18,6 +18,9 @@ def generate_password(pwd_length):
         if (any(char in special_chars for char in pwd) and sum(char in digits for char in pwd) >= 2):
             return pwd
         
+def generate_new_password(pwd_length):
+    return generate_password(pwd_length)
+        
 @app.route("/", methods=["GET", "POST"])
 def password_generator():
     if request.method == "POST":
@@ -25,6 +28,8 @@ def password_generator():
             pwd_length = int(request.form["length"])
             if pwd_length <= 0:
                 raise ValueError("Password length must be a positive integer.")
+            elif pwd_length > 20:
+                raise ValueError("Password length cannot be more than 20.")
 
             password = generate_password(pwd_length)
             return render_template("index.html", password=password)
@@ -32,6 +37,15 @@ def password_generator():
             return render_template("index.html", error=str(e))
 
     return render_template("index.html", password="")
+
+@app.route("/generate-new-password", methods=["POST"])
+def generate_new_password_route():
+    try:
+        pwd_length = 12  # Update with your desired password length
+        password = generate_new_password(pwd_length)  # Generate a new password
+        return jsonify({'password': password})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Return an error response if an exception occurs
 
 if __name__ == "__main__":
     app.run(debug=True)
